d32f11713b46dc7ac7d26d1f19a1e9831f9f7591
Nahid Islam <nahid_islam1997@live.co.uk>
2023-10-23 13:26:48 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-  apiKey: "AIzaSyCshuL3rg4AHKCpB4xrjhAZgFf1IE6WGSs",
-  authDomain: "the-fusion-ad4ed.firebaseapp.com",
-  projectId: "the-fusion-ad4ed",
-  storageBucket: "the-fusion-ad4ed.appspot.com",
-  messagingSenderId: "90965635226",
-  appId: "1:90965635226:web:ee8bbfcdf906cff93eaf23",
-  measurementId: "G-K2W3BZLWDD"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
